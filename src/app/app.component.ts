51c2bddac2c73e4873647d5261c681e5fa248791
kvidakov <vidakovic.karlo@gmail.com>
2021-09-02 23:07:48 +0200

    add main app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { IMatch } from './interfaces/match.interface';
+import { ITicket } from './interfaces/ticket.interface';
+import { ILeague } from './interfaces/league.interface';
+import { SportEventsService } from './services/sport-events.service';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { IMockServerResponse } from './interfaces/mock-server-response.interface';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'supersport';
+  sportEvents$: Observable<IMockServerResponse>;
+  leagues$: Observable<ILeague[]>;
+  ticket$: BehaviorSubject<ITicket> = new BehaviorSubject({ matches: [] });
+
+  constructor(private sportEventsService: SportEventsService) {}
+
+  ngOnInit() {
+    this.getSportEvents();
+  }
+
+  private getSportEvents() {
+    this.sportEvents$ = this.sportEventsService.getSportEvents();
+    this.leagues$ = this.sportEvents$.pipe(
+      map((sportEvents: IMockServerResponse) => {
+        if (!!sportEvents && sportEvents.locations.length) {
+          return sportEvents.locations[0].leagues;
+        }
+        return [];
+      })
+    );
+  }
+
+  onPickClicked(event: IMatch) {
+    const currentTicketValue = this.ticket$.value.matches;
+    const eventIndex = currentTicketValue.findIndex(
+      (match: IMatch) => match.event.id === event.event.id
+    );
+    if (eventIndex === -1) {
+      currentTicketValue.push(event);
+    } else {
+      // tslint:disable-next-line:max-line-length
+      const isSamePick = !!currentTicketValue.find(
+        (match: IMatch) =>
+          match.pick.id === event.pick.id && match.event.id === event.event.id
+      );
+      isSamePick
+        ? currentTicketValue.splice(eventIndex, 1)
+        : currentTicketValue.splice(eventIndex, 1, event);
+    }
+    this.ticket$.next({ matches: currentTicketValue });
+  }
+
+  onRemovePickClicked(event: IMatch) {
+    const currentTicketValue = this.ticket$.value.matches;
+    const eventIndex = currentTicketValue.findIndex(
+      (match: IMatch) => match.event.id === event.event.id
+    );
+    currentTicketValue.splice(eventIndex, 1);
+    this.ticket$.next({ matches: currentTicketValue });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { LeagueSectionComponent } from './components/league/league.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FixtureTableComponent } from './components/fixture-table/fixture-table.component';
+import { TicketComponent } from './components/ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FixtureTableComponent,
+    LeagueSectionComponent,
+    TicketComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
